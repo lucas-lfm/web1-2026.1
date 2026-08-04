55430a74246443601c8e7161b99569db87dce960
Lucas Mendes <mendes.lucas.lfm@gnail.com>
2026-08-03 21:40:46 -0300

    adiciona o exemplo trabalhado nas aulas de 28/07 e 03/08

---
+++ exemplos/ex-07-28/main.js
@@ -0,0 +1,28 @@
+console.log("Hello, World!");
+
+// console.log(idade);
+
+// Criando um bloco de código com chaves {}
+if (true) {
+  let teste = "hahahahaha";
+  console.log(teste);
+}
+
+let idade = 18;
+let nome = "Lucas Mendes";
+
+console.log("A idade do individuo é: " + idade);
+
+// Seleciona um elemento pelo seu ID
+document.getElementById("pnome").innerHTML =
+  "O aluno " + nome + " tem " + idade + " anos de idade";
+
+nome = "Chico da Silva";
+
+const PI = 3.14;
+
+console.log(PI ** 2);
+
+let ultimo_nome; // Snake Case
+let primeiroNome; // Camel Case (Camelo)
+let PrimeiroNome; // Pascal Case
